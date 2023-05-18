165ce473d1036ae1a4e88e95b4d3e29ac102aac1
admin <Hunter.d.burningham@gmail.com>
2023-05-17 20:42:13 -0600

    LogLevels first solution

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -1,19 +1,28 @@
 using System;
+using System.Text.RegularExpressions;
 
 static class LogLine
 {
+
+    private static readonly string pattern = @"\[(\w+)\]:\s(.+)";
+
+     public static string LogLevel(string logLine)
+    {
+        var match = Regex.Match(logLine, pattern); 
+        return match.Groups[1].Value.ToLower();
+    }
+    
     public static string Message(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
-    }
-
-    public static string LogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+        var match = Regex.Match(logLine, pattern);
+        return match.Groups[2].Value.Trim();
     }
 
     public static string Reformat(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+        string trimmedLogLine = logLine.Trim();
+        string message = Message(trimmedLogLine);
+        string logLevel = LogLevel(trimmedLogLine);
+        return $"{message} ({logLevel})";
     }
 }
