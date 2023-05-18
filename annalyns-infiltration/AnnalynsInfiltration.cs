25cf3a9be6de67553ee454422b1f6f1dc97f1230
admin <Hunter.d.burningham@gmail.com>
2023-05-17 19:25:29 -0600

    impl. lambdas & DeMorgan's for readabilty

--- annalyns-infiltration/AnnalynsInfiltration.cs
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -2,51 +2,14 @@ using System;
 
 static class QuestLogic
 {
-    public static bool CanFastAttack(bool knightIsAwake)
-    {
-        if (!knightIsAwake)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    public static bool CanFastAttack(bool knightIsAwake) => !knightIsAwake;
 
-    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
-    {
-        if (knightIsAwake || archerIsAwake || prisonerIsAwake)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) =>
+        knightIsAwake || archerIsAwake || prisonerIsAwake;
 
-    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
-    {
-        if (!archerIsAwake && prisonerIsAwake)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) =>
+        !archerIsAwake && prisonerIsAwake;
 
-    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
-    {
-        if ((prisonerIsAwake && !knightIsAwake && !archerIsAwake) || petDogIsPresent && !archerIsAwake)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) =>
+        !archerIsAwake && (prisonerIsAwake && !knightIsAwake || petDogIsPresent);
 }
